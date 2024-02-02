ab0db23d215d63bc8e4a26d05b31ea987ce4ecda
kirilman <kirilman03@gmail.com>
2024-02-02 19:03:10 +0500

    create setup

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+from setuptools import find_packages
+
+setup(name='asbestutills',
+      version='0.2',
+      description='Python Distribution Utilities',
+      url='https://www.python.org/sigs/distutils-sig/',
+      packages=['labelutilits'], 
+     )
