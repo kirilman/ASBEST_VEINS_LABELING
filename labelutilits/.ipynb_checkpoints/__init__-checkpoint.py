94cf0490ff195427cbd489d4223cf70bedf5433b
kirilman <kirilman03@gmail.com>
2024-02-05 12:24:17 +0500

    rename labelutilits add gitignore

--- labelutilits/.ipynb_checkpoints/__init__-checkpoint.py
+++
@@ -1,3 +0,0 @@
-from ._path import *
-from ._annotation import *
-from ._anno_collector import *
--- labelutilits/__init__.py
+++
@@ -1,5 +0,0 @@
-from ._path import *
-from ._annotation import *
-from ._anno_collector import *
-from ._coords_transition import *
-from ._data_spliter import k_fold_split_yolo
